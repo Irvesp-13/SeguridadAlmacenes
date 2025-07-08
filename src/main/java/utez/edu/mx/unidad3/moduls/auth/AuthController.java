569625750a8e7f500f170821d4bf50245b9d37b7
Irivng Uriel Espinosa Hernandez <20223tn050@utez.edu.mx>
2025-07-07 19:10:43 -0600

    [ADD] Errores solucionados

--- src/main/java/utez/edu/mx/unidad3/moduls/auth/AuthController.java
+++ src/main/java/utez/edu/mx/unidad3/moduls/auth/AuthController.java
@@ -3,6 +3,7 @@ package utez.edu.mx.unidad3.moduls.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import utez.edu.mx.unidad3.moduls.auth.dto.LoginRequestDTO;
@@ -15,7 +16,7 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("")
-    public ResponseEntity<APIResponse> doLogin(LoginRequestDTO payload) {
+    public ResponseEntity<APIResponse> doLogin(@RequestBody LoginRequestDTO payload) {
         APIResponse response = authService.doLogin(payload);
         return new ResponseEntity<>(response, response.getStatus());
     }
--- src/main/java/utez/edu/mx/unidad3/moduls/auth/AuthService.java
+++ src/main/java/utez/edu/mx/unidad3/moduls/auth/AuthService.java
@@ -3,7 +3,6 @@ package utez.edu.mx.unidad3.moduls.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import utez.edu.mx.unidad3.moduls.auth.dto.LoginRequestDTO;
