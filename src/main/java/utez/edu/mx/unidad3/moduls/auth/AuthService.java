41220281a5089f0975e58eb776f781f36594b5dd
Irivng Uriel Espinosa Hernandez <20223tn050@utez.edu.mx>
2025-07-08 18:16:23 -0600

    [ADD] Register de usuarios

--- src/main/java/utez/edu/mx/unidad3/moduls/auth/AuthService.java
+++ src/main/java/utez/edu/mx/unidad3/moduls/auth/AuthService.java
@@ -11,6 +11,9 @@ import utez.edu.mx.unidad3.moduls.user.UserRepository;
 import utez.edu.mx.unidad3.security.jwt.JWTUtils;
 import utez.edu.mx.unidad3.security.jwt.UDService;
 import utez.edu.mx.unidad3.utils.APIResponse;
+import utez.edu.mx.unidad3.utils.PasswordEncoder;
+
+import java.sql.SQLException;
 
 @Service
 public class AuthService {
@@ -26,12 +29,14 @@ public class AuthService {
     @Transactional(readOnly = true)
     public APIResponse doLogin(LoginRequestDTO payload) {
         try {
-            User found = userRepository.findByUsernameAndPassword(
-                    payload.getUsername(),
-                    payload.getPassword()
+            User found = userRepository.findByUsername(
+                    payload.getUsername()
             ).orElse(null);
 
             if (found == null) return new APIResponse("Usuario no encontrado", true, HttpStatus.NOT_FOUND);
+
+            if (! PasswordEncoder.verifyPassword(payload.getPassword(), found.getPassword()))
+                return new APIResponse("Las contraseñas no coinciden", true, HttpStatus.BAD_REQUEST);
 
             UserDetails userDetails = uDService.loadUserByUsername(found.getUsername());
             String token = jwtUtils.generateToken(userDetails);
@@ -46,6 +51,34 @@ public class AuthService {
             ex.printStackTrace();
             return new APIResponse(
                     "Error al iniciar sesión",
+                    true,
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+    @Transactional(rollbackFor = {SQLException.class, Exception.class})
+    public APIResponse register(User payload) {
+        try {
+            if (userRepository.findByUsername(payload.getUsername()).orElse(null) != null)
+                return new APIResponse(
+                        "El nombre de usuario ya existe",
+                        true,
+                        HttpStatus.BAD_REQUEST
+                );
+
+            payload.setPassword(PasswordEncoder.encodePassword(payload.getPassword()));
+            userRepository.save(payload);
+
+            return new APIResponse(
+                    "Operacion exitosa",
+                    false,
+                    HttpStatus.CREATED
+            );
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            return new APIResponse(
+                    "Error al registrar usuario",
                     true,
                     HttpStatus.INTERNAL_SERVER_ERROR
             );
