3736b560e84f98355288c755ae02586e7d223e4c
Antonie <20223TN047@utez.edu.mx>
2025-07-25 23:19:20 -0600

    [ADD] Implementar gestión de roles y validaciones en servicios

---
+++ src/main/java/utez/edu/mx/unidad3/moduls/user/RolRepository.java
@@ -0,0 +1,11 @@
+package utez.edu.mx.unidad3.moduls.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RolRepository extends JpaRepository<Rol, Long> {
+    Optional<Rol> findByName(String name);
+}
